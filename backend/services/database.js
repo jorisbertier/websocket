1dc05c8b34f21c8ad5ed3115dee4c53d6b08d445
Joris Bertier <joris.bertier@gmail.com>
2025-05-06 19:46:32 +0200

    connect to database

---
+++ backend/services/database.js
@@ -0,0 +1,21 @@
+import {MongoClient} from 'mongodb'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGO_URL);
+
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log("✅ Connected to MongoDB");
+        const db = client.db('<dbname>');
+        return db;
+    } catch (error) {
+        console.error("❌ MongoDB connection error:", error);
+    }
+}
+
+connectToDatabase()
+
+export default connectToDatabase;
