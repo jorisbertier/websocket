765d375921b8cd52737075762adcf898509321e7
Joris Bertier <joris.bertier@gmail.com>
2025-06-08 04:15:05 +0200

    change text

--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -49,7 +49,7 @@ export default function MessagesPage() {
             onClick={handleAddFriend}
             className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
           >
-            Ajouter
+            add friend
           </button>
         </div>
       </div>
