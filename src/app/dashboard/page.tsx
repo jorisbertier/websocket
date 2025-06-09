74d59d689b7419bc389bbaf4a54c9b313bf7731b
Joris Bertier <joris.bertier@gmail.com>
2025-06-09 16:08:27 +0200

    create page Dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import AuthGuard from "@/auth/authGuard";
+import Chat from "@/component/chat";
+import { useUser } from "@/hooks/useUser"
+
+
+function Dashboard() {
+    
+    const { user} = useUser();
+    
+    console.log(user)
+
+    console.log('voici connected user:',user)
+    return (
+        <AuthGuard>
+            <div>Bienvenue sur votre dashboard {user?.name}</div>
+        </AuthGuard>
+    )
+}
+
+export default Dashboard
